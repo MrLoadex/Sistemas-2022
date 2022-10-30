6a250220364b3562ca806eb3ccc2f9362c981d2e
MrLoadex <101680130+MrLoadex@users.noreply.github.com>
2022-10-30 18:38:06 -0300

    Correcion errores

--- ALGORITMOS/ALGORITMOS 1/TP_UNIDAD_10_2/build_and_execute_app.bat
+++ ALGORITMOS/ALGORITMOS 1/TP_UNIDAD_10_2/build_and_execute_app.bat
@@ -3,12 +3,10 @@ g++ -Wall -std=c++11 -I.\figura\figura -c .\figura\figura.cpp
 g++ -Wall -std=c++11 -I.\circulo -c .\circulo\circulo.cpp 
 g++ -Wall -std=c++11 -I.\rectangulo -c .\rectangulo\rectangulo.cpp
 g++ -Wall -std=c++11 -I.\triangulo -c .\triangulo\triangulo.cpp
-g++ -Wall -std=c++11 -I.\main -c \main\bd.cpp
-
+g++ -Wall -std=c++11 -I.\main -c .\main\bd.cpp
 
 :: Compilo el Binario
-g++ -I.\figura figura.o -I.\circulo circulo.o -I.\rectangulo rectangulo.o -I.\triangulo triangulo.o .\main\bd.cpp -o application.exe
-
+g++ -I.\figura figura.o -I.\circulo circulo.o -I.\rectangulo rectangulo.o -I.\triangulo triangulo.o -I.\main bd.o -o application.exe
 
 :: Limpio los códigos objeto
 DEL .\*.o
